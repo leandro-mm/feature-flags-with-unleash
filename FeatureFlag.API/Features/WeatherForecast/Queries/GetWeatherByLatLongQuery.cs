13d584acf651c09ff9d36547e41a15574a85dfa4
Leandro <leandro.mm17@gmail.com>
2026-08-27 13:55:12 -0300

    feat: contrução da api

---
+++ FeatureFlag.API/Features/WeatherForecast/Queries/GetWeatherByLatLongQuery.cs
@@ -0,0 +1,7 @@
+
+using FeatureFlag.API.Features.WeatherForecast.DTOs;
+using MediatR;
+
+namespace FeatureFlag.API.Features.WeatherForecast.Queries;
+
+public record GetWeatherByLongLatQuery(double Longitude, double Latitude) : IRequest<WeatherForecastDto>;
